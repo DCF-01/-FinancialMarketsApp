fea237b88cebbee413c4e988bd4174ffc23b2f78
DCF-01 <rkezunovski98@gmail.com>
2022-04-10 15:46:35 +0200

    added core api constants

---
+++ src/FinancialMarketsApp.Core/Interfaces/IAlphaVantageClient.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public interface IAlphaVantageClient
+{
+	public dynamic GetTimeSeries(string timeSpan, string ticker, string interval, bool adjusted, string outputSize, string apiKey)
+}
---
+++ src/FinancialMarketsApp.Infrastructure.AlphaVantage/Constants/Constants.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinancialMarketsApp.Infrastructure.AlphaVantage.Constants
+{
+    public static class Interval
+    {
+        public const string MINUTES_1 = "1min";
+        public const string MINUTES_5 = "5min";
+        public const string MINUTES_15 = "15min";
+        public const string MINUTES_30 = "30min";
+        public const string MINUTES_60 = "60min";
+    }
+
+    public static class TimeSeries
+    {
+        const string TIME_SERIES_INTRADAY = "TIME_SERIES_INTRADAY";
+        const string TIME_SERIES_INTRADAY_EXTENDED = "TIME_SERIES_INTRADAY_EXTENDED";
+        const string TIME_SERIES_DAILY = "TIME_SERIES_DAILY";
+        const string TIME_SERIES_DAILY_ADJUSTED = "TIME_SERIES_DAILY_ADJUSTED";
+        const string TIME_SERIES_WEEKLY = "TIME_SERIES_WEEKLY";
+        const string TIME_SERIES_WEEKLY_ADJUSTED = "TIME_SERIES_WEEKLY_ADJUSTED";
+        const string TIME_SERIES_MONTHLY = "TIME_SERIES_MONTHLY";
+        const string TIME_SERIES_MONTHLY_ADJUSTED = "TIME_SERIES_MONTHLY_ADJUSTED";
+    }
+
+    public static class OutputSize
+    {
+        const string COMPACT = "compact";
+        const string FULL = "full";
+    }
+
+    public static class DataType
+    {
+        const string JSON = "json";
+        const string CSV = "CSV";
+    }
+}

a4a096cb5ef630ee43fd1cbacda81676bf75c9f5
DCF-01 <rkezunovski98@gmail.com>
2022-04-10 15:50:42 +0200

    public modifier

--- src/FinancialMarketsApp.Infrastructure.AlphaVantage/Constants/Constants.cs
+++ src/FinancialMarketsApp.Infrastructure.AlphaVantage/Constants/Constants.cs
@@ -15,25 +15,25 @@ namespace FinancialMarketsApp.Infrastructure.AlphaVantage.Constants
 
     public static class TimeSeries
     {
-        const string TIME_SERIES_INTRADAY = "TIME_SERIES_INTRADAY";
-        const string TIME_SERIES_INTRADAY_EXTENDED = "TIME_SERIES_INTRADAY_EXTENDED";
-        const string TIME_SERIES_DAILY = "TIME_SERIES_DAILY";
-        const string TIME_SERIES_DAILY_ADJUSTED = "TIME_SERIES_DAILY_ADJUSTED";
-        const string TIME_SERIES_WEEKLY = "TIME_SERIES_WEEKLY";
-        const string TIME_SERIES_WEEKLY_ADJUSTED = "TIME_SERIES_WEEKLY_ADJUSTED";
-        const string TIME_SERIES_MONTHLY = "TIME_SERIES_MONTHLY";
-        const string TIME_SERIES_MONTHLY_ADJUSTED = "TIME_SERIES_MONTHLY_ADJUSTED";
+        public const string TIME_SERIES_INTRADAY = "TIME_SERIES_INTRADAY";
+        public const string TIME_SERIES_INTRADAY_EXTENDED = "TIME_SERIES_INTRADAY_EXTENDED";
+        public const string TIME_SERIES_DAILY = "TIME_SERIES_DAILY";
+        public const string TIME_SERIES_DAILY_ADJUSTED = "TIME_SERIES_DAILY_ADJUSTED";
+        public const string TIME_SERIES_WEEKLY = "TIME_SERIES_WEEKLY";
+        public const string TIME_SERIES_WEEKLY_ADJUSTED = "TIME_SERIES_WEEKLY_ADJUSTED";
+        public const string TIME_SERIES_MONTHLY = "TIME_SERIES_MONTHLY";
+        public const string TIME_SERIES_MONTHLY_ADJUSTED = "TIME_SERIES_MONTHLY_ADJUSTED";
     }
 
     public static class OutputSize
     {
-        const string COMPACT = "compact";
-        const string FULL = "full";
+        public const string COMPACT = "compact";
+        public const string FULL = "full";
     }
 
     public static class DataType
     {
-        const string JSON = "json";
-        const string CSV = "CSV";
+        public const string JSON = "json";
+        public const string CSV = "CSV";
     }
 }

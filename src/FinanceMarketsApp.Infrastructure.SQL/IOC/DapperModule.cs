0e07847d7e6082ee8a40e04cfe0304aad480f1d5
Radoslav K <rkezunovski98@gmail.com>
2022-05-15 23:14:46 +0200

    add dapper, sql library proj

---
+++ src/FinanceMarketsApp.Infrastructure.SQL/IOC/DapperModule.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceMarketsApp.Infrastructure.SQL.IOC
+{
+    public static class DapperModule
+    {
+        public static IServiceCollection AddDapperModule(this IServiceCollection services)
+        {
+            services.AddSingleton<DapperContext>();
+            return services;
+        }
+    }
+}
